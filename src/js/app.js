d9693a5cd5eb6313ab3d3fdb27c15ba684fc3d68
Oliver <of9142@my.bristol.ac.uk>
2016-12-15 17:15:24 +0000

    updates for push to heroku

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
-  .constant('API_URL', 'http://localhost:3000/api')
-  // .constant('API_URL', 'http://10.1.7.97:3000/api')
+  .constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000' : '//kickabout-api.herokuapp.com')
 
   .config(Auth);
 
